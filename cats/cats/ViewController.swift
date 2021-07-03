c3929d8f5c318443a1ed39cdf53129216428dc12
TheMichelleGergs <michelle.gergs@holotechnology.co>
2021-07-03 15:07:13 +0200

    create xcode project and install pods

---
+++ cats/cats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cats
+//
+//  Created by michelle gergs on 03/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
